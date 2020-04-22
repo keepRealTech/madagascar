d369aaf1f2bc8facae921af5c860833256f767af
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-04-22 16:02:38 +0800

    Indri image service

---
+++ indri/src/main/java/com/keepreal/madagascar/indri/IndriApplication.java
@@ -0,0 +1,16 @@
+package com.keepreal.madagascar.indri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Represents the main entry for the indri service.
+ */
+@SpringBootApplication
+public class IndriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndriApplication.class, args);
+    }
+
+}
