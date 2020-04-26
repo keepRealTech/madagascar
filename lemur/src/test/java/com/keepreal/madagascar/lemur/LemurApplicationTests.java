8b06ef582d122103a7d0a95307615f797aff7e96
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-04-26 10:18:12 +0800

    add lemur

---
+++ lemur/src/test/java/com/keepreal/madagascar/lemur/LemurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keepreal.madagascar.lemur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
