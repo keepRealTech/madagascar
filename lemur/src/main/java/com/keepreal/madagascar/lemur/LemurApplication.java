b1f70b63ed293b8ab502b9d93cb301725e52eabb
sidneyli-kr <63329438+sidneyli-kr@users.noreply.github.com>
2020-04-26 10:47:28 +0800

    [Madagascar] Update proto (#13)
    * add lemur
    
    * Update proto
    
    * Fix typos
    
    Co-authored-by: sidneyli <sli@appledeMacBook-Pro-5.local>

---
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/LemurApplication.java
@@ -0,0 +1,13 @@
+package com.keepreal.madagascar.lemur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemurApplication.class, args);
+    }
+
+}
