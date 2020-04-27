ac0a0e6a51acf126a6d3366e3bbc38f0b7c1bde0
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-04-27 21:13:16 +0800

    update resource server settings

--- lemur/src/main/java/com/keepreal/madagascar/lemur/config/WebSecurityConfiguration.java
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/config/WebSecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.keepreal.madagascar.lemur.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -23,7 +22,7 @@ public class WebSecurityConfiguration extends ResourceServerConfigurerAdapter {
      * @throws Exception Exception.
      */
     @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
+    public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .cors().and()
                 .csrf().disable()
