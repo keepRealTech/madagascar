8a023425b2b5dc707e36257d879e5de0cddcb12a
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-05-08 16:03:47 +0800

    add tenrecs helm

--- tenrecs/start.sh
+++ tenrecs/start.sh
@@ -1,2 +1,6 @@
+# this is a hack way to set the snowflake node id base on the stateful set ordinal
+pod_index=${POD_NAME##*-}
+export SNOWFLAKE_NODEID=$pod_index
+
 export
 java -cp app:app/lib/* com.keepreal.madagascar.tenrecs.TenrecsApplication
