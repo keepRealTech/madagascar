861fdac9c9f87ef6486552dc5f452516a0aa7da2
sidneyli-kr <63329438+sidneyli-kr@users.noreply.github.com>
2020-05-09 10:47:17 +0800

    Adds resource limits (#34)
    Co-authored-by: sidneyli <sli@appledeMacBook-Pro-5.local>

--- coua/start.sh
+++ coua/start.sh
@@ -3,4 +3,4 @@ pod_index=${HOSTNAME##*-}
 export SNOWFLAKE_NODEID=$pod_index
 
 export
-java -cp app:app/lib/* com.keepreal.madagascar.coua.CouaApplication
+java -Xms2g -Xmx2g -cp app:app/lib/* com.keepreal.madagascar.coua.CouaApplication
