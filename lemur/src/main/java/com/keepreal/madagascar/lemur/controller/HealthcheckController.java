86e83e5df086dcccd7b9674d977264a89ef5e7f7
sidneyli-kr <63329438+sidneyli-kr@users.noreply.github.com>
2020-05-11 13:47:23 +0800

    [Lemur] Adds a healthcheck api for aliyun slb (#38)
    * Adds a healthcheck api for ali slb
    
    * Forbids http
    
    Co-authored-by: sidneyli <sli@appledeMacBook-Pro-5.local>

---
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/controller/HealthcheckController.java
@@ -0,0 +1,24 @@
+package com.keepreal.madagascar.lemur.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import swagger.api.HealthcheckApi;
+import swagger.model.DummyResponse;
+
+/**
+ * Represents the healthcheck controller.
+ */
+@RestController
+public class HealthcheckController implements HealthcheckApi {
+
+    /**
+     * Implements the health check head api.
+     * 
+     * @return {@link HttpStatus}.
+     */
+    public ResponseEntity<DummyResponse> apiHealthcheckHead() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
