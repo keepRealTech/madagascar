c8b664e47ae736314fb96cf4dc48a60eaa7cabf1
zxdong <32349974+958900zh@users.noreply.github.com>
2020-05-13 11:43:31 +0800

    [Madagascar] Add IOS Login (#43)
    * 添加IOS登录
    
    * Update JWTIOSLoginExecutor.java
    
    Co-authored-by: sidneyli-kr <63329438+sidneyli-kr@users.noreply.github.com>

---
+++ baobob/src/main/java/com/keepreal/madagascar/baobob/loginExecutor/model/IOSLoginInfo.java
@@ -0,0 +1,15 @@
+package com.keepreal.madagascar.baobob.loginExecutor.model;
+
+import lombok.Data;
+
+/**
+ * @program: madagascar
+ * @author: zhangxidong
+ * @create: 2020-05-12
+ **/
+
+@Data
+public class IOSLoginInfo {
+    private String fullName;
+    private String unionId;
+}
