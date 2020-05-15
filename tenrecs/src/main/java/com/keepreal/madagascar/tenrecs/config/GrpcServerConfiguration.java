a163078939c9fcb9a00e0491b02a803946f3fdef
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-05-15 14:09:38 +0800

    Add jaeger for tenrecs

---
+++ tenrecs/src/main/java/com/keepreal/madagascar/tenrecs/config/GrpcServerConfiguration.java
@@ -0,0 +1,47 @@
+package com.keepreal.madagascar.tenrecs.config;
+
+import io.grpc.ServerInterceptor;
+import io.opentracing.Tracer;
+import io.opentracing.contrib.grpc.TracingServerInterceptor;
+import org.lognet.springboot.grpc.GRpcGlobalInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Represents the grpc server configuration.
+ */
+@Configuration
+public class GrpcServerConfiguration {
+
+    private final Tracer tracer;
+
+    /**
+     * Constructs the GRpc server configurations.
+     *
+     * @param tracer {@link Tracer}.
+     */
+    public GrpcServerConfiguration(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    /**
+     * Represents the grpc tracing server interceptor.
+     *s
+     * @return {@link TracingServerInterceptor}.
+     */
+    @Bean
+    @ConditionalOnProperty(value = "deploy.opentrace.grpc-server.enabled", havingValue = "true")
+    @GRpcGlobalInterceptor
+    public ServerInterceptor globalServerInterceptor() {
+        return TracingServerInterceptor
+                .newBuilder()
+                .withTracer(this.tracer)
+                .withStreaming()
+                .withVerbosity()
+                .withTracedAttributes(TracingServerInterceptor.ServerRequestAttribute.HEADERS,
+                        TracingServerInterceptor.ServerRequestAttribute.METHOD_TYPE)
+                .build();
+    }
+
+}
