6c5479602673010e107a387a295a8b16a9248d36
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-05-18 20:46:02 +0800

    update locust

--- tools/locust/locustfile.py
+++ tools/locust/locustfile.py
@@ -5,26 +5,25 @@ class TestSet(TaskSet):
 
     token = ""
 
-    def on_start(self):
-        headers = {
-            "Content-Type": "application/json"
-        }
+  #  def on_start(self):
+      #  headers = {
+      #      "Content-Type": "application/json"
+      #  }
 
-        payload = {
-            "loginType": "LOGIN_PASSWORD",
-            "data": { 
-                "username":"user",
-                "password":"pass"
-            }
-        }
+      #  payload = {
+      #      "loginType": "LOGIN_PASSWORD",
+      #      "data": { 
+      #          "username":"user",
+      #          "password":"pass"
+      #      }
+      #  }
 
-        with self.client.post("/api/v1/login", json.dumps(payload), headers = headers, catch_response = True) as response:
-            self.token = json.loads(response.content)['data']['token']
+      #  with self.client.post("/api/v1/login", json.dumps(payload), headers = headers, catch_response = True) as response:
+      #      self.token = json.loads(response.content)['data']['token']
 
     def getConfigs(self):
         self.client.get("/api/v1/configs?configType=IOS")
-
-    @task
+   
     def createFeed(self):
         headers = {
             "Authorization": "bearer " + self.token,
@@ -34,6 +33,17 @@ class TestSet(TaskSet):
         with self.client.post("/api/v1/feeds/6665820807295401984/reposts", json.dumps({ "content": "123", "isSuccessful": True }), headers = headers, catch_response=True) as response:
             if json.loads(response.content)['rtn'] != 0:
                 response.failure("failed")
+    @task
+    def getFeed(self):
+        headers = {
+            "Authorization": "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTA0MDk4NTQsInVzZXJfbmFtZSI6IjAiLCJhdXRob3JpdGllcyI6WyJ1c2VyXzAiXSwianRpIjoiMzIyOGZlNTMtMjc5NC00ODRjLWJmM2MtMTAyMDMwZTRjODhmIiwiY2xpZW50X2lkIjoibGVtdXIiLCJzY29wZSI6WyJhbGwiXX0.uL0WiXW-1UHQaAaK20QEFnKXB6ShP8-_LI1GPUD3hos",
+            "Content-Type": "application/json"
+        }
+   
+        with self.client.get("/api/v1/feeds?islandId=6665811262355542016&fromHost=true", headers = headers, catch_response=True) as response:
+            if json.loads(response.content)['rtn'] != 0:
+                response.failure("failed")
+
 
 class Run(HttpLocust):
     task_set = TestSet
