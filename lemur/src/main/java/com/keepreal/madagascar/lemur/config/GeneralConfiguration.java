d46a421711fdb091d3126e8b4a3460778f0657fb
zxdong <32349974+958900zh@users.noreply.github.com>
2020-05-19 21:13:22 +0800

    [Madagascar] fix bug (#63)
    * 修改测出来的问题
    
    * 添加官方岛id配置

---
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/config/GeneralConfiguration.java
@@ -0,0 +1,17 @@
+package com.keepreal.madagascar.lemur.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * Represents the general configurations.
+ */
+@Configuration
+@ConfigurationProperties(prefix = "general")
+@Data
+public class GeneralConfiguration {
+    private List<String> officialIslandIdList;
+}
