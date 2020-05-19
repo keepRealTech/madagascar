a300231031675c26ac51c42595ad2187e6f682fa
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-05-19 20:54:35 +0800

    Changing image upload to blocking

--- lemur/src/main/java/com/keepreal/madagascar/lemur/service/ImageService.java
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/service/ImageService.java
@@ -1,6 +1,7 @@
 package com.keepreal.madagascar.lemur.service;
 
 import com.google.protobuf.ByteString;
+import com.keepreal.madagascar.common.CommonStatus;
 import com.keepreal.madagascar.common.exceptions.ErrorCode;
 import com.keepreal.madagascar.common.exceptions.KeepRealBusinessException;
 import com.keepreal.madagascar.indri.ImageServiceGrpc;
@@ -8,6 +9,7 @@ import com.keepreal.madagascar.indri.UploadImagesRequest;
 import com.keepreal.madagascar.lemur.util.ImageUtils;
 import io.grpc.Channel;
 import io.grpc.StatusRuntimeException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +21,7 @@ import java.util.Objects;
  * Represents the upload image service.
  */
 @Service
+@Slf4j
 public class ImageService {
 
     private final Channel channel;
@@ -39,21 +42,26 @@ public class ImageService {
      * @return The image uri.
      */
     public String uploadSingleImageAsync(MultipartFile image) {
-        ImageServiceGrpc.ImageServiceFutureStub stub = ImageServiceGrpc.newFutureStub(this.channel);
+        ImageServiceGrpc.ImageServiceBlockingStub stub = ImageServiceGrpc.newBlockingStub(this.channel);
 
         String extension = Objects.requireNonNull(image.getOriginalFilename())
                 .substring(image.getOriginalFilename().lastIndexOf("."));
         String uri = ImageUtils.buildImageUri() + extension;
 
-        UploadImagesRequest request = null;
+        CommonStatus response;
         try {
-            request = UploadImagesRequest.newBuilder()
+            UploadImagesRequest request = UploadImagesRequest.newBuilder()
                     .addImageNames(uri)
                     .addImageContent(ByteString.copyFrom(image.getBytes()))
                     .build();
-            stub.uploadImages(request);
+            response = stub.uploadImages(request);
         } catch (StatusRuntimeException | IOException e) {
             throw new KeepRealBusinessException(ErrorCode.REQUEST_GRPC_IMAGE_UPLOAD_ERROR);
+        }
+
+        if (Objects.isNull(response) || ErrorCode.REQUEST_SUCC_VALUE != response.getRtn()) {
+            log.error("Upload image returned null.");
+            throw new KeepRealBusinessException(ErrorCode.REQUEST_UNEXPECTED_ERROR);
         }
 
         return uri;
