ea65a345f8f82617e69799d1dc37f8fcd4e47877
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-05-28 15:00:59 +0800

    Fix error message

--- lemur/src/main/java/com/keepreal/madagascar/lemur/converter/DefaultErrorMessageTranslater.java
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/converter/DefaultErrorMessageTranslater.java
@@ -33,7 +33,7 @@ public class DefaultErrorMessageTranslater implements ErrorMessageTranslator {
             case REQUEST_ISLAND_NOT_FOUND_ERROR:
                 return "你已不在岛内，请先登岛";
             case REQUEST_GRPC_LOGIN_FROZEN:
-                return "登陆失败，您的账号被暂时锁定";
+                return "登陆失败，你的账号被暂时锁定";
             default:
                 return errorCode.getValueDescriptor().getName();
         }
