6064e868014b6668aad1ee298705c79fa9b6f8e4
zhangxidong <zxdong_55@163.com>
2020-06-01 20:05:03 +0800

    add upload api

--- lemur/src/main/java/com/keepreal/madagascar/lemur/controller/UploadController.java
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/controller/UploadController.java
@@ -1,14 +1,17 @@
 package com.keepreal.madagascar.lemur.controller;
 
+import com.keepreal.madagascar.lemur.service.UploadService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import swagger.api.UploadApi;
+import swagger.model.MediaUrlsRequest;
+import swagger.model.UploadUrlDTO;
 import swagger.model.UploadUrlListResponse;
-import swagger.model.UploadUrlResponse;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * Represents the upload controller.
@@ -16,25 +19,47 @@ import java.util.List;
 @RestController
 public class UploadController implements UploadApi {
 
+    private final UploadService uploadService;
+
     /**
-     * Implements the get image urls api.
+     * Constructs the upload controller.
      *
-     * @param fileNameList fileNameList (required).
-     * @return {@link UploadUrlListResponse}.
+     * @param uploadService {@link UploadService}.
+     */
+    public UploadController(UploadService uploadService) {
+        this.uploadService = uploadService;
+    }
+
+    /**
+     * Implements the media urls api.
+     *
+     * @param mediaUrlsRequest  {@link MediaUrlsRequest}.
+     * @return  {@link UploadUrlListResponse}.
      */
     @Override
-    public ResponseEntity<UploadUrlListResponse> apiV1UploadGetImageUrlsGet(@NotNull @Valid List<String> fileNameList) {
+    public ResponseEntity<UploadUrlListResponse> apiV1UploadMediaUrlsPost(MediaUrlsRequest mediaUrlsRequest) {
+        List<UploadUrlDTO> uploadUrlDTOList = mediaUrlsRequest.getFileNames().stream().map(fileName -> {
+            UploadUrlDTO dto = new UploadUrlDTO();
+            String objectName = generatorObjectName(fileName);
+            dto.setObjectName(objectName);
+            dto.setUrl(uploadService.retrieveUploadUrl(objectName));
+            return dto;
+        }).collect(Collectors.toList());
+
+        UploadUrlListResponse response = new UploadUrlListResponse();
+        response.data(uploadUrlDTOList);
         return null;
     }
 
     /**
-     * Implements the  get video url api.
+     * generator object name by file name(random generator by uuid).
      *
-     * @param fileName fileName (required).
-     * @return  {@link UploadUrlResponse}.
+     * @param fileName  file name.
+     * @return  object name.
      */
-    @Override
-    public ResponseEntity<UploadUrlResponse> apiV1UploadGetVideoUrlGet(@NotNull @Valid String fileName) {
-        return null;
+    private String generatorObjectName(String fileName) {
+        String extension = Objects.requireNonNull(fileName).substring(fileName.lastIndexOf("."));
+        String randomName = UUID.randomUUID().toString().replace("-", "");
+        return randomName + extension;
     }
 }
