8bbef308a77105569070ec7d46c050fd8efc0c97
zxdong <32349974+958900zh@users.noreply.github.com>
2020-06-02 14:45:50 +0800

    [Madagascar] add upload rpc (#113)
    * add upload api
    
    * add fullUserDTO and upload response
    
    * 修改提出来的问题
    
    * 修改变量命名
    
    * add config into helm
    
    * resolve conflict

---
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/service/UploadService.java
@@ -0,0 +1,53 @@
+package com.keepreal.madagascar.lemur.service;
+
+import com.aliyun.oss.HttpMethod;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.model.GeneratePresignedUrlRequest;
+import com.keepreal.madagascar.lemur.config.OssClientConfiguration;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+/**
+ * Represents upload service.
+ */
+@Service
+public class UploadService {
+
+    private final OSS ossClient;
+    private final String bucketName;
+    private final Integer expireTimeInSeconds;
+
+    /**
+     * Constructs the upload service.
+     *
+     * @param ossClient     {@link OSS}.
+     * @param configuration {@link OssClientConfiguration}.
+     */
+    public UploadService(OSS ossClient,
+                         OssClientConfiguration configuration) {
+        this.ossClient = ossClient;
+        this.bucketName = configuration.getBucketName();
+        this.expireTimeInSeconds = configuration.getExpireTimeInSeconds();
+    }
+
+    /**
+     * retrieve upload url
+     *
+     * @param objectName    object name.
+     * @return  upload url.
+     */
+    public String retrieveUploadUrl(String objectName) {
+        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucketName, objectName, HttpMethod.PUT);
+        LocalDateTime localDateTime = LocalDateTime.now().plusSeconds(expireTimeInSeconds);
+        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+        // 设置过期时间。
+        request.setExpiration(date);
+        // 设置Content-Type。
+        request.setContentType("application/octet-stream");
+        // 生成签名URL（HTTP PUT请求）。
+        return ossClient.generatePresignedUrl(request).toString();
+    }
+}
