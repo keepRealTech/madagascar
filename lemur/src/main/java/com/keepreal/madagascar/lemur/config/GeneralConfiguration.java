680696b5576d434b5c73b2db6a752e54f08f04a8
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-06-02 15:07:03 +0800

    Update text filter bean

--- lemur/src/main/java/com/keepreal/madagascar/lemur/config/GeneralConfiguration.java
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/config/GeneralConfiguration.java
@@ -1,7 +1,10 @@
 package com.keepreal.madagascar.lemur.config;
 
+import com.keepreal.madagascar.lemur.textFilter.DefaultContentFilter;
+import com.keepreal.madagascar.lemur.textFilter.TextContentFilter;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
@@ -15,4 +18,15 @@ import java.util.List;
 public class GeneralConfiguration {
     private String singleOfficialIslandId;
     private List<String> officialIslandIdList;
+
+    /**
+     * Initializes the text filter.
+     *
+     * @return {@link TextContentFilter}.
+     */
+    @Bean
+    public TextContentFilter getTextFilter() {
+        return new DefaultContentFilter();
+    }
+
 }
--- lemur/src/main/java/com/keepreal/madagascar/lemur/textFilter/DefaultContentFilter.java
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/textFilter/DefaultContentFilter.java
@@ -1,16 +1,30 @@
 package com.keepreal.madagascar.lemur.textFilter;
 
-import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Set;
 
 /**
  * Represents the default text content filter implementation.
  */
-@Component
 public class DefaultContentFilter implements TextContentFilter {
 
+    private Set<String> dictionary;
+
+    /**
+     * Implementes the basic filter logic.
+     *
+     * @param content Content text.
+     * @return True if not allowed.
+     */
     @Override
     public boolean isDisallowed(String content) {
-        return false;
+        if (StringUtils.isEmpty(content)) {
+            return false;
+        }
+
+        content = content.toUpperCase();
+        return this.dictionary.parallelStream().anyMatch(content::contains);
     }
 
 }
