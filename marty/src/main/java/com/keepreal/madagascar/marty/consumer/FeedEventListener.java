24cd753e888a826b55de97c34db167ffa10abb0b
zhangxidong <zxdong_55@163.com>
2020-06-02 17:50:39 +0800

    create marty & update feed event config

---
+++ marty/src/main/java/com/keepreal/madagascar/marty/consumer/FeedEventListener.java
@@ -0,0 +1,18 @@
+package com.keepreal.madagascar.marty.consumer;
+
+import com.aliyun.openservices.ons.api.Action;
+import com.aliyun.openservices.ons.api.ConsumeContext;
+import com.aliyun.openservices.ons.api.Message;
+import com.aliyun.openservices.ons.api.MessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class FeedEventListener implements MessageListener {
+
+    @Override
+    public Action consume(Message message, ConsumeContext context) {
+        return null;
+    }
+}
