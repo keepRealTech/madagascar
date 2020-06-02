3c04e99479ce448f298d62d1544b8d86da0298cb
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-06-02 19:32:43 +0800

    update parse base64

--- lemur/src/main/java/com/keepreal/madagascar/lemur/textFilter/DefaultContentFilter.java
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/textFilter/DefaultContentFilter.java
@@ -1,11 +1,14 @@
 package com.keepreal.madagascar.lemur.textFilter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -14,9 +17,10 @@ import java.util.stream.Stream;
 /**
  * Represents the default text content filter implementation.
  */
+@Slf4j
 public class DefaultContentFilter implements TextContentFilter {
 
-    private Set<String> dictionary;
+    private Set<String> dictionary = new HashSet<>();
 
     /**
      * Constructs the default content filter.
@@ -24,10 +28,12 @@ public class DefaultContentFilter implements TextContentFilter {
      * @param dictionaryFilePath Dictionary file path.
      */
     public DefaultContentFilter(String dictionaryFilePath) {
-        try (Stream<String> lines = Files.lines(Paths.get(dictionaryFilePath), StandardCharsets.UTF_16)) {
-            this.dictionary = lines.filter(line -> !line.isEmpty()).map(String::toUpperCase).collect(Collectors.toSet());
-        } catch (IOException e) {
-            this.dictionary = new HashSet<>();
+        try {
+            String content = new String(Base64.getDecoder().decode(Files.readAllBytes(Paths.get(dictionaryFilePath))), StandardCharsets.UTF_16);
+            this.dictionary = Arrays.stream(content.split("\r\n")).filter(line -> !line.isEmpty()).map(String::toUpperCase).collect(Collectors.toSet());
+        } catch (IOException ignored) {
+        } finally {
+            log.info("Content filter initialized with {} words.", this.dictionary.size());
         }
     }
 
