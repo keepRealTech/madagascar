2a541488d22ebe8e66befc54b934bca76c6cd30a
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-06-12 15:54:42 +0800

    Upload vanga

--- vanga/start.sh
+++ vanga/start.sh
@@ -1,6 +1,7 @@
 # this is a hack way to set the snowflake node id base on the stateful set ordinal
 pod_index=${HOSTNAME##*-}
 export SNOWFLAKE_NODEID=$pod_index
+export WECHATPAY_HOSTIP=vanga-$pod_index
 
 export
 java -Xms2g -Xmx2g -cp app:app/lib/* com.keepreal.madagascar.vanga.VangaApplication
