13db6f674ff53989fe38d056b17487b456eb4d05
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-06-23 23:08:29 +0800

    update

--- workflow/settler/src/main/java/com/keepreal/madagascar/workflow/settler/service/SettlerService.java
+++ workflow/settler/src/main/java/com/keepreal/madagascar/workflow/settler/service/SettlerService.java
@@ -5,6 +5,7 @@ import com.keepreal.madagascar.workflow.settler.model.Balance;
 import com.keepreal.madagascar.workflow.settler.model.Payment;
 import com.keepreal.madagascar.workflow.settler.model.WorkflowLog;
 import com.keepreal.madagascar.workflow.settler.util.AutoRedisLock;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RedissonClient;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,7 @@ import java.util.stream.Collectors;
  * Represents the settler service.
  */
 @Service
+@Slf4j
 public class SettlerService {
 
     private final WorkflowService workflowService;
@@ -69,6 +71,7 @@ public class SettlerService {
      * @param args Args.
      */
     public void run(String... args) {
+        log.info("Starting workflow [settler].");
         try (AutoRedisLock ignored = new AutoRedisLock(this.redissonClient, "settler", TimeUnit.DAYS.toMillis(1L), 500)) {
             WorkflowLog workflowLog = this.workflowService.initialize();
 
