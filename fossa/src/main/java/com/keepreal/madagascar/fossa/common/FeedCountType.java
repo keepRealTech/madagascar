da91f8da8245f5e7241ad95879195e974a123dfb
zhangxidong <zxdong_55@163.com>
2020-06-24 17:31:52 +0800

    fix repost count

--- fossa/src/main/java/com/keepreal/madagascar/fossa/common/FeedCountType.java
+++ fossa/src/main/java/com/keepreal/madagascar/fossa/common/FeedCountType.java
@@ -9,5 +9,5 @@ package com.keepreal.madagascar.fossa.common;
 public class FeedCountType {
     public static final String LIKES_COUNT = "likesCount";
     public static final String COMMENTS_COUNT = "commentsCount";
-    public static final String REPOST_COUNT = "repostcount";
+    public static final String REPOST_COUNT = "repostCount";
 }
