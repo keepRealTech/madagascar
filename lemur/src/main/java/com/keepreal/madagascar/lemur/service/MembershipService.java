23908fc55977aaff1f919015675baa62fa393622
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-06-29 19:34:30 +0800

    remove

--- lemur/src/main/java/com/keepreal/madagascar/lemur/service/MembershipService.java
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/service/MembershipService.java
@@ -23,7 +23,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Represents the membership service.
