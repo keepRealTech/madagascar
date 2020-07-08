0c8f77a5286d6239c998c3de3a221b5143abce57
sidneyli <sli@appledeMacBook-Pro-5.local>
2020-07-08 10:03:28 +0800

    update

--- asity/src/main/java/com/keepreal/madagascar/asity/grpcController/ChatController.java
+++ asity/src/main/java/com/keepreal/madagascar/asity/grpcController/ChatController.java
@@ -1,10 +1,16 @@
 package com.keepreal.madagascar.asity.grpcController;
 
 import com.keepreal.madagascar.asity.ChatServiceGrpc;
+import com.keepreal.madagascar.asity.EnableChatAccessRequest;
+import com.keepreal.madagascar.asity.IslandChatAccessResponse;
 import com.keepreal.madagascar.asity.RegisterRequest;
 import com.keepreal.madagascar.asity.RegisterResponse;
+import com.keepreal.madagascar.asity.RetrieveChatAccessByIslandIdAndUserIdRequest;
+import com.keepreal.madagascar.asity.model.IslandChatAccess;
+import com.keepreal.madagascar.asity.service.IslandChatAccessService;
 import com.keepreal.madagascar.asity.service.RongCloudService;
 import com.keepreal.madagascar.asity.util.CommonStatusUtils;
+import com.keepreal.madagascar.common.CommonStatus;
 import com.keepreal.madagascar.common.exceptions.ErrorCode;
 import com.keepreal.madagascar.common.exceptions.KeepRealBusinessException;
 import io.grpc.stub.StreamObserver;
@@ -17,14 +23,18 @@ import org.lognet.springboot.grpc.GRpcService;
 public class ChatController extends ChatServiceGrpc.ChatServiceImplBase {
 
     private final RongCloudService rongCloudService;
+    private final IslandChatAccessService islandChatAccessService;
 
     /**
      * Constructs the chat controller.
      *
-     * @param rongCloudService {@link RongCloudService}.
+     * @param rongCloudService        {@link RongCloudService}.
+     * @param islandChatAccessService {@link IslandChatAccessService}.
      */
-    public ChatController(RongCloudService rongCloudService) {
+    public ChatController(RongCloudService rongCloudService,
+                          IslandChatAccessService islandChatAccessService) {
         this.rongCloudService = rongCloudService;
+        this.islandChatAccessService = islandChatAccessService;
     }
 
     /**
@@ -52,6 +62,41 @@ public class ChatController extends ChatServiceGrpc.ChatServiceImplBase {
                     .setStatus(CommonStatusUtils.buildCommonStatus(ErrorCode.REQUEST_UNEXPECTED_ERROR))
                     .build();
         }
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    /**
+     * Enables the island chat access.
+     *
+     * @param request          {@link EnableChatAccessRequest}.
+     * @param responseObserver {@link StreamObserver}.
+     */
+    public void enableChatAccess(EnableChatAccessRequest request,
+                                 StreamObserver<CommonStatus> responseObserver) {
+        this.islandChatAccessService.enable(request.getIslandId(), request.getUserId());
+
+        CommonStatus response = CommonStatusUtils.buildCommonStatus(ErrorCode.REQUEST_SUCC);
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+
+    /**
+     * Retrieves the chat access for an island and user.
+     *
+     * @param request            {@link RetrieveChatAccessByIslandIdAndUserIdRequest}.
+     * @param responseObserver   {@link StreamObserver}.
+     */
+    public void retrieveChatAccessByIslandIdAndUserId(RetrieveChatAccessByIslandIdAndUserIdRequest request,
+                                                      StreamObserver<IslandChatAccessResponse> responseObserver) {
+        IslandChatAccess islandChatAccess = this.islandChatAccessService.createIslandChatAccess(request.getIslandId(), request.getUserId());
+
+        // TODO: FILL THE RESPONSE
+        IslandChatAccessResponse response = IslandChatAccessResponse.newBuilder()
+                .setStatus(CommonStatusUtils.buildCommonStatus(ErrorCode.REQUEST_SUCC))
+                .build();
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
