68d818f5050937f3ac415aa96f3686c04e5201df
sidneyli <liteng@keepreal.group>
2020-07-20 12:18:57 +0800

    update workflow

---
+++ common/workflow/src/main/java/com/keepreal/madagascar/common/workflow/annotation/EnableWorkflowService.java
@@ -0,0 +1,20 @@
+package com.keepreal.madagascar.common.workflow.annotation;
+
+import com.keepreal.madagascar.common.workflow.service.WorkflowService;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Represents the {@link EnableWorkflowService} annotation.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import({WorkflowService.class})
+public @interface EnableWorkflowService {
+}
