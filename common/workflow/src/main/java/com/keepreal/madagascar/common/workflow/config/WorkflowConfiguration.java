6fb2f7e06888dadd44d1f01f720c9218814d3e62
sidneyli <liteng@keepreal.group>
2020-07-20 12:09:18 +0800

    update profile

--- common/workflow/src/main/java/com/keepreal/madagascar/common/workflow/config/WorkflowConfiguration.java
+++ common/workflow/src/main/java/com/keepreal/madagascar/common/workflow/config/WorkflowConfiguration.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Represents the workflow configuration.
+ */
 @Configuration
 @ConfigurationProperties(prefix = "workflow")
 @Data
