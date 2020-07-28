1bf7182a03eea9826cb6fc2b27f619ab1b423fe8
sidneyli <liteng@keepreal.group>
2020-07-28 21:08:11 +0800

    update

--- vanga/src/main/java/com/keepreal/madagascar/vanga/model/IosOrder.java
+++ vanga/src/main/java/com/keepreal/madagascar/vanga/model/IosOrder.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -32,6 +33,7 @@ public class IosOrder {
     @Column(name = "is_deleted")
     @Builder.Default
     private Boolean deleted = false;
+    @CreatedDate
     private Long createdTime;
     @LastModifiedDate
     private Long updatedTime;
