3602118429b443257fa158fa52935a8c48ed0ea1
ChuaN <guoyachuan@keepreal.com>
2020-08-04 16:40:51 +0800

    [baobob] implements serializable

--- baobob/src/main/java/com/keepreal/madagascar/baobob/loginExecutor/model/WechatUserInfo.java
+++ baobob/src/main/java/com/keepreal/madagascar/baobob/loginExecutor/model/WechatUserInfo.java
@@ -4,12 +4,14 @@ import com.keepreal.madagascar.common.Gender;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * Represents the wechat user info.
  */
 @Builder
 @Data
-public class WechatUserInfo {
+public class WechatUserInfo implements Serializable {
 
     private String name;
     private Gender gender;
