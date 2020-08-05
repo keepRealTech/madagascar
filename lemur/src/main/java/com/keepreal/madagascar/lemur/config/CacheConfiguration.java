6fd3a55703969184c877005150b04a4bfd007af0
sidneyli <liteng@keepreal.group>
2020-08-05 12:02:36 +0800

    update

--- lemur/src/main/java/com/keepreal/madagascar/lemur/config/CacheConfiguration.java
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/config/CacheConfiguration.java
@@ -1,17 +1,26 @@
-package com.keepreal.madagascar.lemur.config.grpc;
+package com.keepreal.madagascar.lemur.config;
 
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.jsr107.EhcacheCachingProvider;
+import org.ehcache.xml.XmlConfiguration;
 import org.redisson.api.RedissonClient;
 import org.redisson.spring.cache.CacheConfig;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.jcache.JCacheCacheManager;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * Represents the caching configurations.
@@ -20,19 +29,21 @@ import java.util.Map;
 @Configuration
 public class CacheConfiguration {
 
-    private final JCacheCacheManager echcacheManager;
     private final RedissonClient redissonClient;
 
-    public CacheConfiguration(JCacheCacheManager echcacheManager,
-                              RedissonClient redissonClient) {
-        this.echcacheManager = echcacheManager;
+    public CacheConfiguration(RedissonClient redissonClient) {
         this.redissonClient = redissonClient;
     }
 
     @Bean
     @Primary
-    public CacheManager cacheManager() {
-        return this.echcacheManager;
+    public CacheManager ehCacheCacheManager() throws URISyntaxException {
+        CachingProvider provider = Caching.getCachingProvider(EhcacheCachingProvider.class.getName());
+        javax.cache.CacheManager cacheManager = provider.getCacheManager(
+                Objects.requireNonNull(this.getClass().getClassLoader().getResource("ehcache-3.xml")).toURI(),
+                this.getClass().getClassLoader()
+        );
+        return new JCacheCacheManager(cacheManager);
     }
 
     @Bean
