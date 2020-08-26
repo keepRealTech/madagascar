26251f2a049e05c5e9c66f1248036b53f4459be9
zxdong <32349974+958900zh@users.noreply.github.com>
2020-08-26 17:27:28 +0800

    [Lemur] add feed membership checked dto (#337)
    * add feed membership checked dto
    
    * sort

--- lemur/src/main/java/com/keepreal/madagascar/lemur/dtoFactory/BoxDTOFactory.java
+++ lemur/src/main/java/com/keepreal/madagascar/lemur/dtoFactory/BoxDTOFactory.java
@@ -3,16 +3,22 @@ package com.keepreal.madagascar.lemur.dtoFactory;
 import com.google.protobuf.ProtocolStringList;
 import com.keepreal.madagascar.common.AnswerMessage;
 import com.keepreal.madagascar.common.FeedMessage;
+import com.keepreal.madagascar.coua.FeedMembershipMessage;
 import com.keepreal.madagascar.fossa.BoxMessage;
+import com.keepreal.madagascar.lemur.service.BoxService;
 import com.keepreal.madagascar.lemur.service.MembershipService;
 import com.keepreal.madagascar.lemur.service.SubscribeMembershipService;
 import com.keepreal.madagascar.lemur.service.UserService;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import swagger.model.BoxAccessDTO;
 import swagger.model.BoxDTO;
+import swagger.model.FeedMembershipCheckedDTO;
 import swagger.model.FullQuestionDTO;
 import swagger.model.QuestionDTO;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
@@ -26,19 +32,22 @@ public class BoxDTOFactory {
     private final CommentDTOFactory commentDTOFactory;
     private final MembershipService membershipService;
     private final MembershipDTOFactory membershipDTOFactory;
+    private final BoxService boxService;
 
     public BoxDTOFactory(SubscribeMembershipService subscribeMembershipService,
                          UserService userService,
                          UserDTOFactory userDTOFactory,
                          CommentDTOFactory commentDTOFactory,
                          MembershipService membershipService,
-                         MembershipDTOFactory membershipDTOFactory) {
+                         MembershipDTOFactory membershipDTOFactory,
+                         BoxService boxService) {
         this.subscribeMembershipService = subscribeMembershipService;
         this.userService = userService;
         this.userDTOFactory = userDTOFactory;
         this.commentDTOFactory = commentDTOFactory;
         this.membershipService = membershipService;
         this.membershipDTOFactory = membershipDTOFactory;
+        this.boxService = boxService;
     }
 
     public BoxDTO valueOf(BoxMessage boxMessage, String userId) {
@@ -157,6 +166,46 @@ public class BoxDTOFactory {
                 .collect(Collectors.toList()));
 
         return dto;
+    }
+
+    public List<FeedMembershipCheckedDTO> listValueOf(String islandId) {
+        List<FeedMembershipMessage> feedMembershipMessages = this.membershipService.retrieveFeedMembershipsByIslandId(islandId);
+        BoxMessage boxMessage = this.boxService.retrieveBoxInfo(islandId);
+
+        List<FeedMembershipCheckedDTO> dtoList = new ArrayList<>();
+
+        boolean hasChecked;
+
+        hasChecked = !boxMessage.getEnabled();
+
+        FeedMembershipCheckedDTO dto = new FeedMembershipCheckedDTO();
+        dto.setId("-1");
+        dto.setMembershipName("所有岛民");
+        dto.setChecked(!hasChecked && CollectionUtils.isEmpty(boxMessage.getMembershipIdsList()));
+        hasChecked = hasChecked || CollectionUtils.isEmpty(boxMessage.getMembershipIdsList());
+        dtoList.add(dto);
+
+        boolean checked = hasChecked;
+        List<FeedMembershipCheckedDTO> checkedDTOS = feedMembershipMessages.stream().filter(feedMembershipMessage -> !StringUtils.isEmpty(feedMembershipMessage.getId())).map(feedMembershipMessage -> {
+            FeedMembershipCheckedDTO checkedDTO = new FeedMembershipCheckedDTO();
+            checkedDTO.setId(feedMembershipMessage.getId());
+            checkedDTO.setMembershipName(feedMembershipMessage.getName());
+            checkedDTO.setChargePerMonth(feedMembershipMessage.getPricePerMonth());
+            checkedDTO.setMemberCount(feedMembershipMessage.getMemberCount());
+
+            checkedDTO.setChecked(!checked && boxMessage.getMembershipIdsList().contains(feedMembershipMessage.getId()));
+
+            return checkedDTO;
+        }).collect(Collectors.toList());
+
+        dtoList.addAll(checkedDTOS);
+
+        dto = new FeedMembershipCheckedDTO();
+        dto.setId("-2");
+        dto.setMembershipName("禁止提问");
+        dto.setChecked(!boxMessage.getEnabled());
+        dtoList.add(dto);
+        return dtoList;
     }
 
 }
