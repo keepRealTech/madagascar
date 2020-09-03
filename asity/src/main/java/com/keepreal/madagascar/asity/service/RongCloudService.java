70bea31e1d8756aafc8f484a33fb33f9e061cc59
zxdong <32349974+958900zh@users.noreply.github.com>
2020-09-03 15:59:10 +0800

    update helm and add create island content (#386)

--- asity/src/main/java/com/keepreal/madagascar/asity/service/RongCloudService.java
+++ asity/src/main/java/com/keepreal/madagascar/asity/service/RongCloudService.java
@@ -30,7 +30,16 @@ public class RongCloudService {
 
     private static final String HOST_TEMPLATE = "我刚刚支持了你的「%s」会员（¥%.2f x %d个月）。创作加油！";
     private static final String MEMBER_TEMPLATE = "感谢你的支持！\n更多信息可前往「我的 - 订单中心」中查看。";
-    private final static String OFFICIAL_USER_ID = "4";
+    private static final String OFFICIAL_USER_ID = "4";
+    private static final String CREATE_ISLAND_CONTNET = "成为创作者的私信：\n" +
+            "恭喜你成功创建了你的主页！现在可以在手机端发布动态、网页端上传作品或分享创作主页链接获取支持了。\n" +
+            "\n" +
+            "你还可以：\n" +
+            "1. 前往「我的-主页设置-创作者认证」，申请认证。认证可以防止您被冒充，获得跳岛首页推荐的机会，以及不定期的创作鼓励补贴。\n" +
+            "2. 阅读《创作者指南》，以便更好的使用跳岛。\n" +
+            "\n" +
+            "\n" +
+            "欢迎来官方微博给我留言跟我互动哦～如遇到问题可在设置内-咨询跳岛小客服";
     private final RongCloud client;
 
     /**
@@ -201,7 +210,7 @@ public class RongCloudService {
 
     @SneakyThrows
     public void sentCreateIslandNotice(CreateIslandEvent createIslandEvent) {
-        TxtMessage txtMessage = new TxtMessage("", "");
+        TxtMessage txtMessage = new TxtMessage(CREATE_ISLAND_CONTNET, "");
 
         PrivateMessage message = new PrivateMessage()
                 .setSenderId(OFFICIAL_USER_ID)
