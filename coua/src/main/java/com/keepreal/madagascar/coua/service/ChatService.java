7223afc215a23f052c91b2b4e4a8a78ccf23c552
sidneyli <liteng@keepreal.group>
2020-09-04 16:44:57 +0800

    update

---
+++ coua/src/main/java/com/keepreal/madagascar/coua/service/ChatService.java
@@ -0,0 +1,79 @@
+package com.keepreal.madagascar.coua.service;
+
+import com.google.protobuf.StringValue;
+import com.keepreal.madagascar.asity.ChatServiceGrpc;
+import com.keepreal.madagascar.asity.DeleteChatgroupMembershipByMembershipIdRequest;
+import com.keepreal.madagascar.asity.UpdateRongCloudUserRequest;
+import com.keepreal.madagascar.common.exceptions.ErrorCode;
+import com.keepreal.madagascar.common.exceptions.KeepRealBusinessException;
+import io.grpc.Channel;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+/**
+ * Represents the chatgroup service.
+ */
+@Service
+public class ChatService {
+
+    private final Channel channel;
+
+    /**
+     * Constructs the chatgroup service.
+     *
+     * @param channel GRpc managed channel connection to service Vanga.
+     */
+    public ChatService(@Qualifier("asityChannel") Channel channel) {
+        this.channel = channel;
+    }
+
+    /**
+     * Deletes the chatgroup membership.
+     *
+     * @param membershipId Membership id.
+     */
+    public void deleteChatgroupMembershipByMembershipId(String membershipId) {
+        ChatServiceGrpc.ChatServiceBlockingStub stub = ChatServiceGrpc.newBlockingStub(this.channel);
+
+        DeleteChatgroupMembershipByMembershipIdRequest request =
+                DeleteChatgroupMembershipByMembershipIdRequest.newBuilder().setMemberhsipId(membershipId).build();
+
+        try {
+            stub.deleteChatgroupMembershipByMembershipId(request);
+        } catch (Exception e) {
+            throw new KeepRealBusinessException(ErrorCode.REQUEST_UNEXPECTED_ERROR);
+        }
+    }
+
+    /**
+     * Updates the rong cloud user info.
+     *
+     * @param userId           User id.
+     * @param userName         User name.
+     * @param portraitImageUri User portrait.
+     */
+    public void updateRongCloudUserInfo(String userId, String userName, String portraitImageUri) {
+        ChatServiceGrpc.ChatServiceBlockingStub stub = ChatServiceGrpc.newBlockingStub(this.channel);
+
+        UpdateRongCloudUserRequest.Builder requestBuilder = UpdateRongCloudUserRequest
+                .newBuilder()
+                .setUserId(userId);
+
+        if (Objects.nonNull(userName)) {
+            requestBuilder.setName(StringValue.of(userName));
+        }
+
+        if (Objects.nonNull(portraitImageUri)) {
+            requestBuilder.setPortraitImageUri(StringValue.of(portraitImageUri));
+        }
+
+        try {
+            stub.updateRongCloudUser(requestBuilder.build());
+        } catch (Exception e) {
+            throw new KeepRealBusinessException(ErrorCode.REQUEST_UNEXPECTED_ERROR);
+        }
+    }
+
+}
