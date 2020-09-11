ef590ff078489a2ee2605890b614965b6b7779be
zhangxidong <zxdong_55@163.com>
2020-09-11 15:05:44 +0800

    add support activity

---
+++ workflow/supportActivity/src/main/java/com/keepreal/madagascar/workflow/supportActivity/model/MaxAmount.java
@@ -0,0 +1,6 @@
+package com.keepreal.madagascar.workflow.supportActivity.model;
+
+public interface MaxAmount {
+
+    Long getAmount();
+}
---
+++ workflow/supportActivity/src/main/java/com/keepreal/madagascar/workflow/supportActivity/service/SupportActivityService.java
@@ -0,0 +1,44 @@
+package com.keepreal.madagascar.workflow.supportActivity.service;
+
+import com.keepreal.madagascar.common.snowflake.generator.LongIdGenerator;
+import com.keepreal.madagascar.workflow.supportActivity.model.MaxAmount;
+import com.keepreal.madagascar.workflow.supportActivity.model.SupportActivity;
+import com.keepreal.madagascar.workflow.supportActivity.repository.SupportActivityRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SupportActivityService {
+
+    private final SupportActivityRepository supportActivityRepository;
+    private final LongIdGenerator idGenerator;
+
+    private final static int MAX_CENTS = 5000;
+
+    public SupportActivityService(SupportActivityRepository supportActivityRepository,
+                                  LongIdGenerator idGenerator) {
+        this.supportActivityRepository = supportActivityRepository;
+        this.idGenerator = idGenerator;
+    }
+
+    public void process() {
+        List<String> payeeIdList = this.supportActivityRepository.findAllPayeeId();
+        payeeIdList.forEach(payeeId -> {
+            List<MaxAmount> maxAmounts = this.supportActivityRepository.findByUserId(payeeId);
+            long sum = maxAmounts.stream().mapToLong(amount -> amount.getAmount() > MAX_CENTS ? MAX_CENTS : amount.getAmount()).sum();
+            this.save(payeeId, sum);
+        });
+    }
+
+    private void save(String userId, Long amount) {
+        SupportActivity supportActivity = this.supportActivityRepository.findSupportActivityByUserIdAndDeletedIsFalse(userId);
+        if (supportActivity == null) {
+            supportActivity.setId(String.valueOf(idGenerator.nextId()));
+            supportActivity.setUserId(userId);
+        }
+
+        supportActivity.setAmount(amount);
+        this.supportActivityRepository.save(supportActivity);
+    }
+}
