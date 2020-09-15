fff5d92cce7c1a1d253e2bc473c5d10287cbe66c
zxdong <32349974+958900zh@users.noreply.github.com>
2020-09-15 19:16:48 +0800

    [workflow] update payment (#417)
    * add workflow update payment
    
    * add workflow update payment
    
    * add model

---
+++ workflow/updatePayment/src/main/java/com/keepreal/madagascar/workflow/updatePayment/model/WechatOrder.java
@@ -0,0 +1,13 @@
+package com.keepreal.madagascar.workflow.updatePayment.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class WechatOrder {
+
+    @javax.persistence.Id
+    private String Id;
+}
