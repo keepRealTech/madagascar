b6b1b5b77912639d1e957d0b022a90dac432649f
zhangxidong <zxdong_55@163.com>
2020-09-23 12:01:47 +0800

    update start.sh

--- workflow/support-activity/start.sh
+++ workflow/support-activity/start.sh
@@ -1,2 +1,2 @@
 export
-java -Xms1g -Xmx1g -cp app:app/lib/* com.keepreal.madagascar.workflow.supportActivity.SupportActivityApplication
+java -Xms1g -Xmx1g -cp app:app/lib/* com.keepreal.madagascar.workflow.support_activity.SupportActivityApplication
