2b47041019646184c752e93399287dc35f34096a
sidneyli <liteng@keepreal.group>
2020-10-14 17:41:05 +0800

    update asity

--- common/constants/src/main/java/com/keepreal/madagascar/common/constants/Templates.java
+++ common/constants/src/main/java/com/keepreal/madagascar/common/constants/Templates.java
@@ -8,13 +8,13 @@ public class Templates {
     /**
      * First system feed after island creation.
      */
-    public static final String DEFAULT_FEED_TEXT = "于yyyy年MM月dd日HH:mm，创建了属于我的岛。";
+    public static final String FOSSA_DEFAULT_FEED_TEXT = "于yyyy年MM月dd日HH:mm，创建了属于我的岛。";
 
     /**
      * Advanced publish hints.
      */
-    public static final String POSTING_INSTRUCTION_TITLE = "高清原图、长篇文章、音频和视频发布指南:";
-    public static final String POSTING_INSTRUCTION_CONTENT = "1.在电脑端打开跳岛官网 tiaodaoapp.com\r\n" +
+    public static final String LEMUR_POSTING_INSTRUCTION_TITLE = "高清原图、长篇文章、音频和视频发布指南:";
+    public static final String LEMUR_POSTING_INSTRUCTION_CONTENT = "1.在电脑端打开跳岛官网 tiaodaoapp.com\r\n" +
             "2.微信扫码登录\r\n" +
             "3.点击\"发布\"按钮";
 
