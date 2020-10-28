e0255293c00c706fee19624ddeaa497d1b794477
zhangxidong <zxdong_55@163.com>
2020-10-28 20:49:37 +0800

    add income

---
+++ vanga/src/main/java/com/keepreal/madagascar/vanga/model/IncomeDetail.java
@@ -0,0 +1,41 @@
+package com.keepreal.madagascar.vanga.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Builder
+@Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "income_detail")
+public class IncomeDetail {
+
+    @Id
+    private String id;
+    private String userId;
+    private Long monthTimestamp;
+    private Long amountInCents;
+    private Integer supportCount;
+    @Column(name = "is_deleted")
+    @Builder.Default
+    private Boolean deleted = false;
+    @CreatedDate
+    private Long createdTime;
+    @LastModifiedDate
+    private Long updatedTime;
+}
