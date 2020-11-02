caf1ed724b0e6570e000e12ab199bb3fe2bc71a1
sidneyli <liteng@keepreal.group>
2020-11-02 20:29:42 +0800

    fix

--- fossa/src/main/java/com/keepreal/madagascar/fossa/model/AudioInfo.java
+++ fossa/src/main/java/com/keepreal/madagascar/fossa/model/AudioInfo.java
@@ -1,8 +1,10 @@
 package com.keepreal.madagascar.fossa.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class AudioInfo extends MediaInfo {
     private String url;
     private String title;
--- fossa/src/main/java/com/keepreal/madagascar/fossa/model/VideoInfo.java
+++ fossa/src/main/java/com/keepreal/madagascar/fossa/model/VideoInfo.java
@@ -1,8 +1,10 @@
 package com.keepreal.madagascar.fossa.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class VideoInfo extends MediaInfo {
 
     private String url;
